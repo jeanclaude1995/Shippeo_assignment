91fa50b77074763b11185a90aebbec9372af79e9
jeanclaude1995 <77775226+jeanclaude1995@users.noreply.github.com>
2022-12-19 22:17:26 +0100

    2nd commit

--- shippeo/app.py
+++ shippeo/app.py
@@ -1,4 +1,5 @@
 import psycopg2
+import zlib
 def find_common_character(list):
     value_priority=[]
     for i in list:
@@ -22,13 +23,15 @@ def find_common_character(list):
 filePath="input_file.txt"
 list=[]
 with open(filePath,'r') as file:
+    #data = file.read().rstrip()
     var=file.read().replace('\n', ',')
     list=var.split(',')
-print(list)
 result=find_common_character(list)
 print("The sum of the value of the common characters is: "+ str(result))
 
 
+with open(filePath, 'r') as f:
+    data=f.read().rstrip()
 
 # Connect to the database
 conn = psycopg2.connect(host="postgres", user="postgres", password="mypassword", dbname="postgres")
@@ -37,10 +40,10 @@ conn = psycopg2.connect(host="postgres", user="postgres", password="mypassword",
 cur = conn.cursor()
 
 #Create a table in the database with execution date of the script and the result of the script
-cur.execute("CREATE TABLE IF NOT EXISTS logs (id SERIAL PRIMARY KEY, date TIMESTAMP,  result VARCHAR(255))")
+cur.execute("CREATE TABLE IF NOT EXISTS logs (id SERIAL PRIMARY KEY, date TIMESTAMP,content TEXT,result VARCHAR(255))")
 
 #Insert the execution date of the script ,list and the result of the script into the table 
-cur.execute("INSERT INTO logs (date,result) VALUES (now(), %s)", (result,))
+cur.execute("INSERT INTO logs (date,content,result) VALUES (now(), %s,%s)", (data, result,))
 
 # Commit the changes to the database
 conn.commit()
