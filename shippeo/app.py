e6669b7914a96a2fbdcd259f5c414f19222053f8
jeanclaude1995 <77775226+jeanclaude1995@users.noreply.github.com>
2022-12-20 17:19:36 +0100

    4th commit

--- shippeo/app.py
+++ shippeo/app.py
@@ -1,50 +1,46 @@
 import psycopg2
+import sys
+import zlib
 def find_common_character(list):
-    value_priority=[]
+    total=0
     for i in list:
-        total=len(i)
-        half=total//2
-        first=i[:half]
-        second=i[half:]
+        half=len(i)//2
         #find the common character
-        common=""
-        for i in first:
-            if i in second and i not in common:
-                common+=i
+        common=set(i[:half]).intersection(set(i[half:]))
+        common=common.pop()
         if common.islower():
-            value_priority.append(ord(common)-96)
+            total+=ord(common)-96
         else:
-            value_priority.append(ord(common)-38)
-    return sum(value_priority)
+            total+=ord(common)-38
+    return total
 
-filePath="input_file.txt"
-list=[]
-with open(filePath,'r') as file:
-    var=file.read().replace('\n', ',')
-    list=var.split(',')
-result=find_common_character(list)
-print("The sum of the value of the common characters is: "+ str(result))
-
-
-with open(filePath, 'r') as f:
-    data=f.read().rstrip()
-
-
-# Connect to the database
-conn = psycopg2.connect(host="postgres", user="postgres", password="mypassword", dbname="postgres")
-
-# Create a cursor
-cur = conn.cursor()
-
-#Creating a table in the database with execution date of the script ,content and the result of the script
-cur.execute("CREATE TABLE IF NOT EXISTS logs (id SERIAL PRIMARY KEY, date TIMESTAMP,content TEXT,result VARCHAR(255))")
-
-#Insert the execution date of the script ,data and the result of the script into the table 
-cur.execute("INSERT INTO logs (date,content,result) VALUES (now(), %s,%s)", (data, result,))
-
-# Commit the changes to the database
-conn.commit()
-
-# Close the cursor and connection
-cur.close()
-conn.close()
+def write_log_to_db(result,data):
+    # database connections
+    conn = psycopg2.connect(host="postgres", user="postgres", password="mypassword", dbname="postgres")
+    # Create a cursor
+    cur = conn.cursor()
+    #Creating a table in the database with execution date of the script ,content and the result of the script
+    cur.execute("CREATE TABLE IF NOT EXISTS logs (id SERIAL PRIMARY KEY, date TIMESTAMP,content TEXT,result VARCHAR(255))")
+    #Insert the execution date of the script ,data and the result of the script into the table 
+    cur.execute("INSERT INTO logs (date,content,result) VALUES (now(), %s,%s)", (data, result,))
+    # Commit the changes to the database
+    conn.commit()
+    # Close the cursor and connection
+    cur.close()
+    conn.close()
+        
+#main function
+if __name__ == '__main__':
+    filePath=sys.argv[1]
+    list=[]
+    with open(filePath,'r') as file:
+        text=file.read()
+        var=text.replace('\n', ',')
+        list=var.split(',')
+    result=find_common_character(list)
+    print("The sum of the value of the common characters is: "+ str(result))
+    #compress the text
+    data=zlib.compress(text.encode('utf-8'))
+    print("The compressed text is: "+ str(data))
+    write_log_to_db(result,data)
+    
